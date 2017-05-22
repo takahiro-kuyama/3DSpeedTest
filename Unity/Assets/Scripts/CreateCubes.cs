2fc9cc589591dcf09bee64c02031c4eb22753087
taka_pikopiko <rolling.child.20th@gmail.com>
2017-05-22 16:02:10 +0900

    Make three Samples
    make three samples 'MakeManyCubes', 'MoveManyCubes', and 'NoObjectScene'

--- Unity/Assets/Scripts/CreateCubes.cs
+++ Unity/Assets/Scripts/CreateCubes.cs
@@ -5,6 +5,7 @@ using System;
 
 public class CreateCubes : MonoBehaviour {
     public int cubeNum = 1000;
+    public bool moveCube = false;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +15,9 @@ public class CreateCubes : MonoBehaviour {
             cube.transform.position = new Vector3((float)(rand.NextDouble() * 10 - 5), (float)(rand.NextDouble() * 10 - 5), (float)(rand.NextDouble() * 10 - 5));
             cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
             cube.transform.parent = this.transform;
+            if (moveCube) {
+                cube.AddComponent<MovingCube>();
+            }
         }
     }
 }
---
+++ Unity/Assets/Scripts/MovingCube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour {
+    enum MovingAxis {x,y,z};
+
+    public float movePeriod = 2.0f;
+    public float amplitude = 0.2f;
+
+    private Vector3 initialPosition;
+    private MovingAxis movingAxis;
+
+	// Use this for initialization
+	void Start () {
+        movingAxis = (MovingAxis)Random.Range(0, 3);
+        initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveDegree = amplitude * Mathf.Sin(Time.realtimeSinceStartup / movePeriod * 2 * Mathf.PI);
+        switch (movingAxis) {
+            case MovingAxis.x:
+                transform.position = new Vector3(initialPosition.x + moveDegree, initialPosition.y, initialPosition.z);
+                break;
+            case MovingAxis.y:
+                transform.position = new Vector3(initialPosition.x, initialPosition.y + moveDegree, initialPosition.z);
+                break;
+            case MovingAxis.z:
+                transform.position = new Vector3(initialPosition.x, initialPosition.y, initialPosition.z + moveDegree);
+                break;
+        }
+	}
+}
